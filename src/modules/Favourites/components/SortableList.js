f020d7e75cf8a15b172222a946685f143b1c7b70
Dylan Shaw <dylanshaw012@gmail.com>
2017-09-02 23:52:52 +1000

    Update sortableList

--- src/modules/Favourites/components/SortableList.js
+++ src/modules/Favourites/components/SortableList.js
@@ -1,52 +1,136 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types';
-import { TouchableHighlight, TouchableOpacity, Text, View, StyleSheet, Platform } from "react-native";
+import PropTypes from "prop-types";
+import {
+  TouchableHighlight,
+  TouchableOpacity,
+  Text,
+  View,
+  StyleSheet,
+  Platform
+} from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 
 export default class RenderRowComponent extends Component {
-  renderIcon = () => {
-    if(Platform.OS === 'ios') {
+  renderRemoveIcon = () => {
+    if (Platform.OS === "ios") {
       return (
-        <Icon style={{ flex: 0 }} name="ios-remove-circle" size={22} color="red" />
-      )
+        <View>
+          <Icon
+            style={{ flex: 0 }}
+            name="ios-remove-circle"
+            size={24}
+            color="red"
+          />
+        </View>
+      );
     } else {
       return (
-        <Icon style={{ flex: 0 }} name="md-remove-circle" size={22} color="red" />
-      )
+        <View>
+          <Icon
+            style={{ flex: 0 }}
+            name="md-remove-circle"
+            size={24}
+            color="red"
+          />
+        </View>
+      );
     }
-  }
+  };
+
+  renderReorderIcon = () => {
+    if (Platform.OS === "ios") {
+      return (
+        <View>
+          <Icon
+            style={{ flex: 0 }}
+            name="ios-reorder-outline"
+            size={32}
+            color="#3e4450"
+          />
+        </View>
+      );
+    } else {
+      return (
+        <View>
+          <Icon
+            style={{ flex: 0 }}
+            name="md-reorder"
+            size={32}
+            color="#3e445"
+          />
+        </View>
+      );
+    }
+  };
 
   render() {
     const { departStop, arrivStop, id } = this.props.data;
     if (this.props) {
       return (
-        <TouchableHighlight
-          underlayColor={"#eee"}
-          style={{
-            padding: 25,
-            backgroundColor: "#F8F8F8",
-            borderBottomWidth: 1,
-            borderColor: "#eee"
-          }}
-          {...this.props.sortHandlers}
-        >
-          <View style={{ flexDirection: 'row'}}>
-            <Text style={{ flex: 1 }}>
-              {this.props.data ? departStop : null} -{" "}
-              {this.props.data ? arrivStop : null}
-            </Text>
-            <TouchableOpacity onPress={() => this.props.removeJourney(id)}>
-              {this.renderIcon()}
-            </TouchableOpacity>
+        <View style={styles.itemContainer}>
+          <View style={styles.headerItem}>
+            <View style={styles.headerRow}>
+              <TouchableOpacity
+                style={styles.icon}
+                onPress={() => this.props.removeJourney(id)}
+              >
+                {this.renderRemoveIcon()}
+              </TouchableOpacity>
+              <View>
+                <Text style={styles.titleText}>
+                  {this.props.data ? departStop : null}
+                </Text>
+                <Text style={styles.titleText}>
+                  {this.props.data ? arrivStop : null}
+                </Text>
+              </View>
+            </View>
           </View>
-        </TouchableHighlight>
+          <TouchableOpacity {...this.props.sortHandlers}>
+            {this.renderReorderIcon()}
+          </TouchableOpacity>
+        </View>
       );
     }
   }
 }
 
+const styles = StyleSheet.create({
+  itemContainer: {
+    padding: 15,
+    backgroundColor: "#F8F8F8",
+    borderBottomWidth: 1,
+    borderColor: "#eee",
+    marginLeft: 15,
+    marginRight: 15,
+    marginTop: 10,
+
+    borderRadius: 4,
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  headerItem: {
+    flex: 1
+  },
+  headerRow: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  titleText: {
+    color: "#3e4450",
+    fontWeight: "500",
+    fontSize: 13
+  },
+  icon: {
+    width: 30
+  },
+  flexRow: {
+    flexDirection: "row"
+  }
+});
+
 RenderRowComponent.propTypes = {
   removeJourney: PropTypes.func.isRequired,
   sortHandlers: PropTypes.object,
   data: PropTypes.object.isRequired
-}
+};
