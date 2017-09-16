c291c88ff956bea9fad1c4885c6127048b205b32
Dylan Shaw <dylanshaw012@gmail.com>
2017-09-16 21:37:49 +1000

    Add icon to no search results component

--- src/modules/NewJourney/components/NoSearchResults.js
+++ src/modules/NewJourney/components/NoSearchResults.js
@@ -1,15 +1,42 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Platform } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const NoSearchResults = props => {
+
+  renderResultsIcon = () => {
+    if(Platform.OS === "ios") {
+      return (
+        <Icon name="ios-sad-outline" size={60} color="#ebebeb" />
+      )
+    } else {
+      return (
+        <Icon name="md-sad" size={60} color="#ebebeb" />
+      )
+    }
+  }
+
   return (
-    <View>
-      <Text>No Results :(</Text>
+    <View style={styles.container}>
+      {renderResultsIcon()}
+      <Text style={styles.noResultsText}>No Search Results</Text>
     </View>
   )
 }
 
-NoSearchResults.propTypes = {}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "flex-start"
+  },
+  noResultsText: {
+    marginTop: (Platform.OS === "ios") ? 0 : 10,
+    color: "#ebebeb",
+    fontSize: 18
+  }
+})
 
 export default NoSearchResults;
