73d2f6efe30805d1caf45c47e0f4d80d9dab2024
Dylan Shaw <dylanshaw012@gmail.com>
2017-09-16 23:07:08 +1000

    Edit no search results component

--- src/modules/NewJourney/components/NoSearchResults.js
+++ src/modules/NewJourney/components/NoSearchResults.js
@@ -1,29 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { StyleSheet, Text, View, Platform } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 
 const NoSearchResults = props => {
-
   renderResultsIcon = () => {
-    if(Platform.OS === "ios") {
-      return (
-        <Icon name="ios-sad-outline" size={60} color="#ebebeb" />
-      )
+    if (Platform.OS === "ios") {
+      return <Icon name="ios-sad-outline" size={90} color="#ebebeb" />;
     } else {
-      return (
-        <Icon name="md-sad" size={60} color="#ebebeb" />
-      )
+      return <Icon name="md-sad" size={60} color="#ebebeb" />;
     }
-  }
+  };
 
   return (
     <View style={styles.container}>
       {renderResultsIcon()}
       <Text style={styles.noResultsText}>No Search Results</Text>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -33,10 +28,10 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start"
   },
   noResultsText: {
-    marginTop: (Platform.OS === "ios") ? 0 : 10,
+    marginTop: Platform.OS === "ios" ? 0 : 10,
     color: "#ebebeb",
     fontSize: 18
   }
-})
+});
 
 export default NoSearchResults;
