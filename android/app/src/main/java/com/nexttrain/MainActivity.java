97a8329768afba16a09918d475292e2d1041bf29
Dylan Shaw <dylanshaw012@gmail.com>
2018-02-16 23:02:48 +1100

    Changed and simplified android splash screen to remove white flash

--- android/app/src/main/java/com/nexttrain/MainActivity.java
+++ android/app/src/main/java/com/nexttrain/MainActivity.java
@@ -1,7 +1,29 @@
 package com.nexttrain;
 
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.util.TypedValue;
+
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
+  @Override
+  public LinearLayout createSplashLayout() {
+    LinearLayout view = new LinearLayout(this);
+    TextView textView = new TextView(this);
+
+    view.setBackgroundColor(Color.parseColor("#0ca794"));
+    view.setGravity(Gravity.CENTER);
+
+    textView.setTextColor(Color.parseColor("#FFFFFF"));
+    textView.setText("Loading...");
+    textView.setGravity(Gravity.CENTER);
+    textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 24);
+
+    view.addView(textView);
+    return view;
+  }
 
 }
