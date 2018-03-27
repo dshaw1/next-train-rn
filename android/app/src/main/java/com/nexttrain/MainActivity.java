655e425e69e8e669fb2910ded5ed0b2fe4bd4b8a
Dylan Shaw <dylanshaw012@gmail.com>
2018-03-27 21:03:06 +1100

    Messing around with android splash screen

--- android/app/src/main/java/com/nexttrain/MainActivity.java
+++ android/app/src/main/java/com/nexttrain/MainActivity.java
@@ -2,9 +2,9 @@ package com.nexttrain;
 
 import android.widget.LinearLayout;
 import android.graphics.Color;
-import android.widget.TextView;
+//import android.widget.TextView;
 import android.view.Gravity;
-import android.util.TypedValue;
+//import android.util.TypedValue;
 
 import com.reactnativenavigation.controllers.SplashActivity;
 
@@ -12,17 +12,17 @@ public class MainActivity extends SplashActivity {
   @Override
   public LinearLayout createSplashLayout() {
     LinearLayout view = new LinearLayout(this);
-    TextView textView = new TextView(this);
+//    TextView textView = new TextView(this);
 
     view.setBackgroundColor(Color.parseColor("#0ca794"));
     view.setGravity(Gravity.CENTER);
-
-    textView.setTextColor(Color.parseColor("#FFFFFF"));
-    textView.setText("Loading Next Train...");
-    textView.setGravity(Gravity.CENTER);
-    textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 24);
-
-    view.addView(textView);
+//
+//    textView.setTextColor(Color.parseColor("#FFFFFF"));
+//    textView.setText("Loading Next Train...");
+//    textView.setGravity(Gravity.CENTER);
+//    textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 24);
+//
+//    view.addView(textView);
     return view;
   }
 
